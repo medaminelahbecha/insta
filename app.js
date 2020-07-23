65b1ac364b06842ea9130b39da8517eba6cb578b
medaminelahbecha <mohamedamine.lahbecha@esprit.tn>
2020-07-23 18:11:52 +0200

    amine

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const app = express()
 const PORT=process.env.PORT || 5000
 const mongoose = require('mongoose')
-const {MONGOURI} = require('./keys')
+const {MONGOURI} = require('./config/keys')
 
 
 
@@ -29,6 +29,10 @@ app.use(require('./routes/user'))
 
 if(process.env.NODE_ENV=="production"){
     app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
 }
 
 app.listen(PORT,()=>{
